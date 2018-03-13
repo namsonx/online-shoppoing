501049c6d44a1c10ddc7876bdda6eee65c24ad77
APAC\MAS2HC <son.maidinhnam@vn.bosch.com>
2018-03-13 14:40:22 +0700

    Initial

---
+++ onlinebackend/src/main/java/com/namson/onlinebackend/App.java
@@ -0,0 +1,13 @@
+package com.namson.onlinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
